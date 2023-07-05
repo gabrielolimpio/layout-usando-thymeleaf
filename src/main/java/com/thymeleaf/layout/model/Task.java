11e8a7607796b51de8e01c8a3851c1fb5e8cab41
Gabriel Olimpio <gabriel.rochaleao@gmail.com>
2023-07-05 16:02:32 -0300

    projeto usando thymleaf

---
+++ src/main/java/com/thymeleaf/layout/model/Task.java
@@ -0,0 +1,47 @@
+package com.thymeleaf.layout.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Task {
+
+    private Long id;
+    private String name;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private Date date;
+
+    public Task(Long id, String name, Date date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Task(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
